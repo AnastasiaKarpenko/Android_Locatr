b0312050e5150f11e55904d21f6368ecf807d264
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-03-06 11:39:47 +0200

    hooking up locatr fragment

--- app/src/main/java/ws/tilda/anastasia/locatr/LocatrActivity.java
+++ app/src/main/java/ws/tilda/anastasia/locatr/LocatrActivity.java
@@ -1,9 +1,15 @@
 package ws.tilda.anastasia.locatr;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
-public class LocatrActivity extends AppCompatActivity {
+public class LocatrActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return LocatrFragment.newInstance();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
