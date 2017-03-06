ff76111362ad6d17e27d984f34b9675160923646
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-03-06 12:19:15 +0200

    Listening for connection events

--- app/src/main/java/ws/tilda/anastasia/locatr/LocatrFragment.java
+++ app/src/main/java/ws/tilda/anastasia/locatr/LocatrFragment.java
@@ -1,6 +1,7 @@
 package ws.tilda.anastasia.locatr;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
@@ -29,6 +31,23 @@ public class LocatrFragment extends Fragment {
 
         mClient = new GoogleApiClient.Builder(getActivity())
                 .addApi(LocationServices.API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(@Nullable Bundle bundle) {
+                        getActivity().invalidateOptionsMenu();
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                })
                 .build();
     }
 
