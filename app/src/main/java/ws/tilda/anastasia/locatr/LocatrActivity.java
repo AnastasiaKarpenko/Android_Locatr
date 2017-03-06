44bdee037451233e19f7f39c87dfbbfadd5b3978
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-03-06 11:51:07 +0200

    Adding Play Services check

--- app/src/main/java/ws/tilda/anastasia/locatr/LocatrActivity.java
+++ app/src/main/java/ws/tilda/anastasia/locatr/LocatrActivity.java
@@ -1,9 +1,15 @@
 package ws.tilda.anastasia.locatr;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
 public class LocatrActivity extends SingleFragmentActivity {
+    public static final int REQUEST_ERROR = 0;
 
     @Override
     protected Fragment createFragment() {
@@ -11,8 +17,24 @@ public class LocatrActivity extends SingleFragmentActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_locatr);
+    protected void onResume() {
+        super.onResume();
+
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int errorCode = apiAvailability.isGooglePlayServicesAvailable(this);
+
+        if(errorCode != ConnectionResult.SUCCESS) {
+            Dialog errorDialog = apiAvailability
+                    .getErrorDialog(this, errorCode, REQUEST_ERROR,
+                            new DialogInterface.OnCancelListener() {
+                                @Override
+                                public void onCancel(DialogInterface dialog) {
+                                    // Leave if services are unavailable.
+                                    finish();
+                                }
+                            });
+
+            errorDialog.show();
+        }
     }
 }
